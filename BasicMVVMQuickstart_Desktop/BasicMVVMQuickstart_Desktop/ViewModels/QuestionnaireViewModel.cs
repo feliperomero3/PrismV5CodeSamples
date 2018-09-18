3169e14ccfd730d1437727ae786b7e647962faa0
Felipe Romero <feliperomero3@outlook.com>
2018-09-18 12:32:31 -0600

    Remove ViewModelLocator support in favor to declare ViewModel explicitly on each View.

--- BasicMVVMQuickstart_Desktop/BasicMVVMQuickstart_Desktop/ViewModels/QuestionnaireViewModel.cs
+++ BasicMVVMQuickstart_Desktop/BasicMVVMQuickstart_Desktop/ViewModels/QuestionnaireViewModel.cs
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
-using BasicMVVMQuickstart_Desktop.Model;
 using Microsoft.Practices.Prism.Mvvm;
+using BasicMVVMQuickstart_Desktop.Model;
 
 namespace BasicMVVMQuickstart_Desktop.ViewModels
 {
--- BasicMVVMQuickstart_Desktop/BasicMVVMQuickstart_Desktop/Views/MainWindow.xaml.cs
+++ BasicMVVMQuickstart_Desktop/BasicMVVMQuickstart_Desktop/Views/MainWindow.xaml.cs
@@ -1,15 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 
 using System.Windows;
-using Microsoft.Practices.Prism.Mvvm;
-using Microsoft.Practices.Prism;
 
 namespace BasicMVVMQuickstart_Desktop.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, IView
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
